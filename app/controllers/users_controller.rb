2933ce9a6a838353342565f5021fc6d413a567ef
wanhl1990 <wanhl1990@126.com>
2017-11-18 20:23:32 +0800

    Add a sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_url 
+    else
+      render 'new'
+    end
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  #get '/signup', to: 'users/new'
+  resources :users
   
 end
