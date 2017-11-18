b1cde78cb794b048373434e535dd594f7d0aa8dc
wanhl <wanhl1990@126.com>
2017-11-18 21:34:03 +0800

    Add signup  create and new signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,24 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new(user_params)
-    
-    if @user.save
-      redirect_url 
-    else
-      render 'new'
-    end
+    @user = User.new
   end
   
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @user = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the sample app"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
   
   private
