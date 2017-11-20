4bf704c2c69876d284546959de21b1a81bb74d38
wanhl <wanhl1990@126.com>
2017-11-20 21:58:48 +0800

    login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash.now[:success] = "Welcome to sample App"
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combinaton'
+      render 'new'
+    end
+  end
+
+  def destroy
+    logged_out
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get '/signup', to: 'users#new'
 
   root 'static_pages#home'
@@ -9,6 +11,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
   
 end
