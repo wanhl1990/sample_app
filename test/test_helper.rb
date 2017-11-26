c6e928671766ad64ac238ec6489debe46139ae8a
wanhl <wanhl1990@126.com>
2017-11-26 21:51:22 +0800

    Add advance login function, but rails test has some errors

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,20 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+  #
+
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+  class ActionDispatch::IntergrationTest
+
+    def log_in_as(user, password: 'password', remember_me: '1')
+      post login_path, params: {session: {email: "wanhl1990@126.com", password: "wan123456", remember_me: '1'}}
+    end
+  end
 end
